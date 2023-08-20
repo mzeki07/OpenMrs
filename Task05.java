fe0941e993db292ca7e0c0388e50faa0db681479
cavour <cavourlafleur@outlook.com>
2023-08-20 13:13:48 -0400

    Cavour 2nd commit Project

--- Task05.java
+++ Task05.java
@@ -1,2 +1,27 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Task05 {
+    public static void main(String[] args) {
+
+        /*
+     Verify if you are on the home page.
+
+         */
+        String web ;
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+        WebElement loginButton = driver.findElement(By.id("loginButton"));
+
+        if (loginButton.isDisplayed()) {
+            System.out.println("Page opened successfully.");
+        } else {
+            System.out.println("Page opening failed.");
+        }
+
+
+
+        }
 }

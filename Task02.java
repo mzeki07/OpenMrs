abe2f62ccab16987fd0c5d3ce41fc9516f074337
cavour <cavourlafleur@outlook.com>
2023-08-20 13:02:11 -0400

    Cavour 1st commit Project

--- Task02.java
+++ Task02.java
@@ -1,5 +1,38 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import javax.swing.text.Element;
+
 public class Task02 {
+
+    /*
+ Verify if you are able to login by entering the valid username and valid password without choosing a location.
+verify that the error message is "You must choose a location!"
+
+     */
     public static void main(String[] args) {
-        System.out.printf("cavour test");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://demo.openmrs.org/openmrs/login.htm");
+        driver.findElement(By.id("username")).sendKeys("Admin");
+        driver.findElement(By.id("password")).sendKeys("Admin123");
+        driver.findElement(By.id("loginButton")).click();
+        WebElement error = driver.findElement(By.id("sessionLocationError"));
+        if(error.isDisplayed()){
+            System.out.println("True");
+
+        }
+        else {
+            System.out.println("false");
+        }
+
+
+
+
+
+
+
     }
 }

32a210d1f5cc340e809e4cd8d1e91cdb8c00128e
KamaryaEsrapil <138307647+KamaryaEsrapil@users.noreply.github.com>
2023-08-20 14:27:09 +0800

    firstTask

--- Task/Task01.java
+++ Task/Task01.java
@@ -2,13 +2,25 @@ package Task;
 
 import Method.BaseDriver;
 
-public class Task01 {
-    public class Main extends BaseDriver {
+public class Task01 extends BaseDriver {
+
         public static void main(String[] args) {
 
             driver.get("https://demo.openmrs.org/openmrs/login.htm");
+
+            String expectedUrl = "https://demo.openmrs.org/openmrs/login.htm";
+            String actualUrl = driver.getCurrentUrl();
+
+            if (actualUrl.equals(expectedUrl)) {
+                System.out.println("You are on the correct page.");
+            } else {
+                System.out.println("You are on a different page.");
+            }
+
+            // Close the browser
             driver.quit();
+
 
         }
     }
-}
+

581f6fc8c953293436b8fae35df0ed5ace441f38
zeki <zekiursavas07@gmail.com>
2023-08-20 12:46:43 -0400

    trying to pull

--- MethodLogin.java
+++ MethodLogin.java
@@ -3,7 +3,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class MethodLogin_Wait {
+public class MethodLogin {
     public static void loginMethod(){
         WebDriver driver = new ChromeDriver();
         driver.get("https://demo.openmrs.org/openmrs/login.htm");

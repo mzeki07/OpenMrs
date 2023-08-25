f85963a2727e9107dd0a8b5f07a99d12d74f8736
zeki <zekiursavas07@gmail.com>
2023-08-24 20:24:49 -0400

    Task8-9-10 recentered trying to merge

--- Task/Task08.java
+++ Task/Task08.java
@@ -1,10 +1,6 @@
 package Task;
 
-import org.checkerframework.checker.units.qual.A;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Task08 {
 
--- Task/Task09.java
+++ Task/Task09.java
@@ -1,9 +1,6 @@
 package Task;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 
 public class Task09 {
 
